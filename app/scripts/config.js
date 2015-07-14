7ae361ff9b36fbee1c74466791813b280650483d
Abiee Alejandro Echamea Márquez <abiee.alejandro@gmail.com>
2015-07-13 19:39:09 -0500

    Bump dependencies

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,11 @@
 System.config({
   "baseURL": "/scripts/",
   "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
   "paths": {
     "*": "*.js",
     "github:*": "../../jspm_packages/github/*.js",
@@ -10,20 +15,34 @@ System.config({
 
 System.config({
   "map": {
-    "backbone": "npm:backbone@1.1.2",
-    "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
-    "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
-    "bootstrap": "github:twbs/bootstrap@3.3.4",
-    "handlebars": "github:components/handlebars.js@3.0.0",
-    "jquery": "github:components/jquery@2.1.3",
-    "marionette": "github:marionettejs/backbone.marionette@2.4.1",
-    "underscore": "npm:underscore@1.8.2",
+    "babel": "npm:babel-core@5.6.20",
+    "babel-runtime": "npm:babel-runtime@5.6.20",
+    "backbone": "npm:backbone@1.2.1",
+    "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.8",
+    "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.3",
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
+    "core-js": "npm:core-js@0.9.18",
+    "handlebars": "github:components/handlebars.js@3.0.3",
+    "jquery": "github:components/jquery@2.1.4",
+    "marionette": "github:marionettejs/backbone.marionette@2.4.2",
+    "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:backbone@1.1.2": {
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
+    },
+    "npm:babel-runtime@5.6.20": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:backbone@1.2.1": {
       "process": "github:jspm/nodelibs-process@0.1.1",
-      "underscore": "npm:underscore@1.8.2"
+      "underscore": "npm:underscore@1.8.3"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
